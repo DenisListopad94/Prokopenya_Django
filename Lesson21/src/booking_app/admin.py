e5d72b584c2bfea442f4f412648cd33a3e6c78cc
Aliaksandr <prokopenya_a@mail.ru>
2024-05-16 23:07:44 +0300

    1st/2nd/4th points done

--- Lesson21/src/booking_app/admin.py
+++ Lesson21/src/booking_app/admin.py
@@ -2,6 +2,28 @@ from django.contrib import admin
 from .models import Person, Profile, Hotels, HotelsComment, HotelOwner, Hobby, BookInfo, User, PersonComment
 # Register your models here.
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email"]
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["first_name", "last_name", "age", "sex"],
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["city", "email"],
+            },
+        ),
+    ]
+    search_fields = ["first_name", "last_name"]
+    search_help_text = "Поиск осуществлятся по имени и фамилии, а также email"
+
+
 admin.site.register(Person)
 admin.site.register(Profile)
 admin.site.register(Hotels)
@@ -9,6 +31,6 @@ admin.site.register(HotelsComment)
 admin.site.register(HotelOwner)
 admin.site.register(Hobby)
 admin.site.register(BookInfo)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(PersonComment)
 
